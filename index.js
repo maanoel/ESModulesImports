1218a36b7b80fc9ff83d9011ad6ae0046a8659d1
maanoel <manoelvitorbrito@gmail.com>
2022-11-22 21:04:37 -0300

    [FEAT] Adding alias to a function exported with as

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const circleArea = (r) => 3.14 * r ** 2;
 const squareArea = (s) => s * s;
+const sum = (num1, num2) => num1 + num2;
 
-export { circleArea, squareArea };
+export { circleArea, squareArea, sum as soma };
--- index_test.js
+++ index_test.js
@@ -1,4 +1,5 @@
-import { circleArea, squareArea } from './index.js';
+import { circleArea, squareArea, soma } from './index.js';
 
 console.log(circleArea(1));
 console.log(squareArea(5));
+console.log(soma(1, 5));
